d3c4c38ee193ed6b03a71583d4f5150ab6de0326
Christopher Gladney <christophergladney282@gmail.com>
2024-04-02 09:59:06 +0100

    Added all available squares to white's pieces with the exception of en passant and castling

--- utils/movementsFunctions.js
+++ utils/movementsFunctions.js
@@ -120,11 +120,6 @@ export function calculateSpacesWhiteCanMove(pieceObject) {
       }
     }
 
-    // console.log(fileSquaresToCheckToRight, "<<< files to right");
-    // console.log(fileSquaresToCheckToLeft, "<<< files to left");
-    // console.log(numberSquaresToCheckAsc, "<<< numbers ascending");
-    // console.log(numberSquaresToCheckDes, "<<< numbers descending");
-
     for (let i = 0; i < fileSquaresToCheckToRight.length; i++) {
       if (emptySquares.includes(fileSquaresToCheckToRight[i])) {
         availableSquaresToReturn.push(fileSquaresToCheckToRight[i]);
@@ -388,6 +383,8 @@ export function calculateSpacesWhiteCanMove(pieceObject) {
         break;
       }
     }
+
+    return availableSquaresToReturn;
   }
 
   if (pieceObject.piece === "white-king") {
@@ -467,8 +464,6 @@ export function calculateSpacesWhiteCanMove(pieceObject) {
       );
     }
 
-    console.log(squaresToCheck, "<<< squares to check");
-
     for (let i = 0; i < squaresToCheck.length; i++) {
       if (
         emptySquares.includes(squaresToCheck[i]) ||
@@ -482,6 +477,207 @@ export function calculateSpacesWhiteCanMove(pieceObject) {
   }
 
   if (pieceObject.piece === "white-queen") {
-    console.log("queen");
+    const availableSquaresToReturn = [];
+
+    const fileSquaresToCheckToLeft = [];
+    const fileSquaresToCheckToRight = [];
+
+    const numberSquaresToCheckAsc = [];
+    const numberSquaresToCheckDes = [];
+
+    if (Number(currentSquare[1]) < 8) {
+      for (let number = Number(currentSquare[1]) + 1; number < 9; number++) {
+        numberSquaresToCheckAsc.push(`${currentSquare[0]}${number}`);
+      }
+    }
+
+    if (Number(currentSquare[1]) > 1) {
+      for (let number = Number(currentSquare[1]) - 1; number > 0; number--) {
+        numberSquaresToCheckDes.push(`${currentSquare[0]}${number}`);
+      }
+    }
+
+    if (files.indexOf(currentSquare[0]) < 8) {
+      for (let file = files.indexOf(currentSquare[0]) - 1; file > -1; file--) {
+        fileSquaresToCheckToLeft.push(
+          `${files[file]}${Number(currentSquare[1])}`
+        );
+      }
+    }
+
+    if (files.indexOf(currentSquare[0]) > -1) {
+      for (let file = files.indexOf(currentSquare[0]) + 1; file < 8; file++) {
+        fileSquaresToCheckToRight.push(
+          `${files[file]}${Number(currentSquare[1])}`
+        );
+      }
+    }
+
+    for (let i = 0; i < fileSquaresToCheckToRight.length; i++) {
+      if (emptySquares.includes(fileSquaresToCheckToRight[i])) {
+        availableSquaresToReturn.push(fileSquaresToCheckToRight[i]);
+      } else if (blackOccupiedSquares.includes(fileSquaresToCheckToRight[i])) {
+        availableSquaresToReturn.push(fileSquaresToCheckToRight[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < fileSquaresToCheckToLeft.length; i++) {
+      if (emptySquares.includes(fileSquaresToCheckToLeft[i])) {
+        availableSquaresToReturn.push(fileSquaresToCheckToLeft[i]);
+      } else if (blackOccupiedSquares.includes(fileSquaresToCheckToLeft[i])) {
+        availableSquaresToReturn.push(fileSquaresToCheckToLeft[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < numberSquaresToCheckAsc.length; i++) {
+      if (emptySquares.includes(numberSquaresToCheckAsc[i])) {
+        availableSquaresToReturn.push(numberSquaresToCheckAsc[i]);
+      } else if (blackOccupiedSquares.includes(numberSquaresToCheckAsc[i])) {
+        availableSquaresToReturn.push(numberSquaresToCheckAsc[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < numberSquaresToCheckDes.length; i++) {
+      if (emptySquares.includes(numberSquaresToCheckDes[i])) {
+        availableSquaresToReturn.push(numberSquaresToCheckDes[i]);
+      } else if (blackOccupiedSquares.includes(numberSquaresToCheckDes[i])) {
+        availableSquaresToReturn.push(numberSquaresToCheckDes[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    const squaresToCheckLeftUp = [];
+    const squaresToCheckRightUp = [];
+    const squaresToCheckRightDown = [];
+    const squaresToCheckLeftDown = [];
+
+    // Checking diagonally left and up
+    if (files.indexOf(currentSquare[0]) > 0 && Number(currentSquare[1]) < 8) {
+      let squareToMutate = currentSquare;
+      while (
+        files.indexOf(squareToMutate[0]) > 0 &&
+        Number(squareToMutate[1]) < 8
+      ) {
+        squaresToCheckLeftUp.push(
+          `${files[files.indexOf(squareToMutate[0]) - 1]}${
+            Number(squareToMutate[1]) + 1
+          }`
+        );
+        squareToMutate = `${files[files.indexOf(squareToMutate[0]) - 1]}${
+          Number(squareToMutate[1]) + 1
+        }`;
+      }
+    }
+
+    // Checking diagonally right and up
+    if (files.indexOf(currentSquare[0]) < 7 && Number(currentSquare[1]) < 8) {
+      let squareToMutate = currentSquare;
+      while (
+        files.indexOf(squareToMutate[0]) < 8 &&
+        Number(squareToMutate[1]) < 8
+      ) {
+        squaresToCheckRightUp.push(
+          `${files[files.indexOf(squareToMutate[0]) + 1]}${
+            Number(squareToMutate[1]) + 1
+          }`
+        );
+        squareToMutate = `${files[files.indexOf(squareToMutate[0]) + 1]}${
+          Number(squareToMutate[1]) + 1
+        }`;
+      }
+    }
+
+    // Checking diagonally right and down
+    if (files.indexOf(currentSquare[0]) < 7 && Number(currentSquare[1]) > 0) {
+      let squareToMutate = currentSquare;
+      while (
+        files.indexOf(squareToMutate[0]) < 8 &&
+        Number(squareToMutate[1]) > 1
+      ) {
+        squaresToCheckRightDown.push(
+          `${files[files.indexOf(squareToMutate[0]) + 1]}${
+            Number(squareToMutate[1]) - 1
+          }`
+        );
+        squareToMutate = `${files[files.indexOf(squareToMutate[0]) + 1]}${
+          Number(squareToMutate[1]) - 1
+        }`;
+      }
+    }
+
+    // Checking diagonally left and down
+    if (files.indexOf(currentSquare[0]) > 1 && Number(currentSquare[1]) > 0) {
+      let squareToMutate = currentSquare;
+      while (
+        files.indexOf(squareToMutate[0]) > 0 &&
+        Number(squareToMutate[1]) > 1
+      ) {
+        squaresToCheckLeftDown.push(
+          `${files[files.indexOf(squareToMutate[0]) - 1]}${
+            Number(squareToMutate[1]) - 1
+          }`
+        );
+        squareToMutate = `${files[files.indexOf(squareToMutate[0]) - 1]}${
+          Number(squareToMutate[1]) - 1
+        }`;
+      }
+    }
+
+    for (let i = 0; i < squaresToCheckLeftUp.length; i++) {
+      if (emptySquares.includes(squaresToCheckLeftUp[i])) {
+        availableSquaresToReturn.push(squaresToCheckLeftUp[i]);
+      } else if (blackOccupiedSquares.includes(squaresToCheckLeftUp[i])) {
+        availableSquaresToReturn.push(squaresToCheckLeftUp[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < squaresToCheckLeftDown.length; i++) {
+      if (emptySquares.includes(squaresToCheckLeftDown[i])) {
+        availableSquaresToReturn.push(squaresToCheckLeftDown[i]);
+      } else if (blackOccupiedSquares.includes(squaresToCheckLeftDown[i])) {
+        availableSquaresToReturn.push(squaresToCheckLeftDown[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < squaresToCheckRightUp.length; i++) {
+      if (emptySquares.includes(squaresToCheckRightUp[i])) {
+        availableSquaresToReturn.push(squaresToCheckRightUp[i]);
+      } else if (blackOccupiedSquares.includes(squaresToCheckRightUp[i])) {
+        availableSquaresToReturn.push(squaresToCheckRightUp[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    for (let i = 0; i < squaresToCheckRightDown.length; i++) {
+      if (emptySquares.includes(squaresToCheckRightDown[i])) {
+        availableSquaresToReturn.push(squaresToCheckRightDown[i]);
+      } else if (blackOccupiedSquares.includes(squaresToCheckRightDown[i])) {
+        availableSquaresToReturn.push(squaresToCheckRightDown[i]);
+        break;
+      } else {
+        break;
+      }
+    }
+
+    return availableSquaresToReturn;
   }
 }
